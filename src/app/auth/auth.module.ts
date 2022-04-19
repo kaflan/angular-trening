316cb383745e8bff3dbf58520b4c5166b06631ef
Andrii Katrusha <df117879@gmail.com>
2022-04-19 23:00:35 +0300

    fix

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core'
+import {CommonModule} from '@angular/common'
+import {StoreModule} from '@ngrx/store'
 
+import {RegisterComponent} from 'src/app/auth/components/register/register.component'
+import {RouterModule} from '@angular/router'
+import {ReactiveFormsModule} from '@angular/forms'
+import {reducers} from 'src/app/auth/store/reducers'
 
+const routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers)
+  ],
+  declarations: [RegisterComponent]
 })
-export class AuthModule { }
+export class AuthModule {}
