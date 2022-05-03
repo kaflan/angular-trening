29618aa2d113adc836aa436ef69bb80916d20a95
Andrii Katrusha <df117879@gmail.com>
2022-05-03 15:52:11 +0300

    fix your component

---
+++ src/app/your-feed/your-feed.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { YourFeedComponent } from './your-feed.component';
+
+
+
+@NgModule({
+  declarations: [
+    YourFeedComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class YourFeedModule { }
