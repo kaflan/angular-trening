4a7a67a056c0cc5ea27343fe58e373c14a1d7fab
Andrii Katrusha <df117879@gmail.com>
2022-05-12 13:37:30 +0300

    add artiles

---
+++ src/app/shared/services/articles.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
+
+import { environment } from 'src/environments/environment'
+import { GetArticleResponseInterface } from 'src/app/shared/types/getArticleResponse.interface'
+
+@Injectable()
+export class ArticlesService {
+  constructor(private http: HttpClient) {}
+
+  getFeed(url: string): Observable<GetArticleResponseInterface> {
+    return this.http.get<GetArticleResponseInterface>(`${environment.apiUrl}${url}`)
+  }
+}
