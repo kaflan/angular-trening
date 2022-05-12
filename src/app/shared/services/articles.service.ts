ada945fea93565c903d6ef1c66f46293d0140a5e
Andrii Katrusha <df117879@gmail.com>
2022-05-12 13:45:10 +0300

    ArticlesService

--- src/app/shared/services/articles.service.ts
+++ src/app/shared/services/articles.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 import { HttpClient } from '@angular/common/http'
 
 import { environment } from 'src/environments/environment'
 import { GetArticleResponseInterface } from 'src/app/shared/types/getArticleResponse.interface'
+import { ArticleInterface } from 'src/app/shared/types/article.interface'
 
 @Injectable()
 export class ArticlesService {
   constructor(private http: HttpClient) {}
 
-  getFeed(slug: string): Observable<GetArticleResponseInterface> {
-    return this.http.get<GetArticleResponseInterface>(`${environment.apiUrl}/articles/${slug}`)
+  getFeed(slug: string): Observable<ArticleInterface> {
+    return this.http
+      .get<GetArticleResponseInterface>(
+        `${environment.apiUrl}/articles/${slug}`
+      )
+      .pipe(
+        map((response: GetArticleResponseInterface) => {
+          return response.article
+        })
+      )
   }
 }
