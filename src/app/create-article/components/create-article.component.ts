4f9f99ec2759f8faf7d6b8b479f95a8759127953
Andrii Katrusha <df117879@gmail.com>
2022-05-22 16:07:08 +0300

    create article

--- src/app/create-article/components/create-article.component.ts
+++ src/app/create-article/components/create-article.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ArticleInputInterface } from 'src/app/shared/types/articleInput.interface'
 
 @Component({
   selector: 'mc-create-article',
   templateUrl: './create-article.component.html',
-  styleUrls: ['./create-article.component.scss']
+  styleUrls: ['./create-article.component.scss'],
 })
 export class CreateArticleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  initialValues: ArticleInputInterface = {
+    title: 'Foo',
+    description: 'Bar',
+    body: 'Baz',
+    tagList: ['123'],
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit(res: any): void {
+    console.log('onSubmit in pareent', res)
+  }
 }
